4039198e820ffe6117d43c969f64bb3a37ef8413
Ethan Rosenberg <erosenberg2019@hotmail.com>
2024-03-15 12:26:12 -0700

    Moved constants to separate header file

---
+++ TTT-V3/Constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define NAME "TTT-V3"
+#define WIN_WIDTH  1920
+#define WIN_HEIGHT 1080
+#define WIN_RATIO  1.77	// width / height 
+#define FOV 45.0f
+#define AA_SAMPLES 8
+
+#endif
--- TTT-V3/main.h
+++ TTT-V3/main.h
@@ -5,13 +5,8 @@
 #include "Display.h"
 #include "CubeMap.h"
 #include "Skybox.h"
+#include "Constants.h"
 #include <glad/glad.h>
-
-#define NAME "TTT-V3"
-#define WIN_WIDTH  1920
-#define WIN_HEIGHT 1080
-#define WIN_RATIO  1.77	// width / height 
-#define FOV 45.0f
 
 std::string readFile(const char* filename);
 
